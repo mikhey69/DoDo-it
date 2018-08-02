4a8a739c352c1cc22d499daf05e5a81ffd0d3f2d
mikhey <mikheenkovladislav@gmail.com>
2018-08-02 13:30:51 +0300

    Initial Commit

---
+++ DoDo itTests/DoDo_itTests.swift
@@ -0,0 +1,36 @@
+//
+//  DoDo_itTests.swift
+//  DoDo itTests
+//
+//  Created by mikhey on 02.08.2018.
+//  Copyright © 2018 mikhey. All rights reserved.
+//
+
+import XCTest
+@testable import DoDo_it
+
+class DoDo_itTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
